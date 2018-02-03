3e433d565cf8b1f888229a068b487d397745e4a7
Benyam Ephrem <ephremdsg@gmail.com>
2018-02-03 14:43:32 -0500

    tamper route added to demonstrate what happens when data is tampered with

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const Blockchain = require('../objects/blockchain');
 const Block = require('../objects/block');
 const Data = require('../objects/data');
 
-const DataBlockChain = new Blockchain();
+const dataBlockChain = new Blockchain();
 
 // GET '/'
 router.get('/', function(req, res, next) {
@@ -21,23 +21,53 @@ router.get('/', function(req, res, next) {
     // and alot more helper functions to help brevity
 
 
-    DataBlockChain.addBlock(new Block(1, 1517542370, [new Data("ben", "ephrem")], DataBlockChain.getLatestBlock().hash));
-    DataBlockChain.addBlock(new Block(2, 1517542578, [new Data("michael", "wein")], DataBlockChain.getLatestBlock().hash));
-    DataBlockChain.addBlock(new Block(3, 1517543621, [new Data("john", "lon"), new Data("suzy", "doozy")], DataBlockChain.getLatestBlock().hash));
+    dataBlockChain.addBlock(new Block(1, 1517542370, [new Data("ben", "ephrem")], dataBlockChain.getLatestBlock().hash));
+    dataBlockChain.addBlock(new Block(2, 1517542578, [new Data("michael", "wein")], dataBlockChain.getLatestBlock().hash));
+    dataBlockChain.addBlock(new Block(3, 1517543621, [new Data("john", "lon"), new Data("suzy", "doozy")], dataBlockChain.getLatestBlock().hash));
 
     // Set headers and send the example response
     res.setHeader('Content-Type', 'application/json');
-    return res.send(JSON.stringify(DataBlockChain));
+    return res.send(JSON.stringify(dataBlockChain));
 });
 
 // GET '/breakchain'
 router.get('/breakchain', function(req, res, next) {
 
     // Invalidate chain by deleting the 2nd block (index to delete, amount of indicies to delete from that index on)
-    DataBlockChain.chain.splice(1, 1);
+    dataBlockChain.chain.splice(1, 1);
 
     // Get the result of validation
-    const validationResult = DataBlockChain.isChainValid();
+    const validationResult = dataBlockChain.isChainValid();
+
+    // Set header type
+    res.setHeader('Content-Type', 'application/json');
+
+    // If the blockchain is valid return true
+    // If the blockchain is invalid the result will be false and the invalid index will be pointed out
+    if(validationResult.result === true){
+        return res.json({
+            result: validationResult.result
+        });
+    } else{
+        return res.json({
+            result: validationResult.result,
+            invalidBlocks: validationResult.invalidBlocks
+        });
+    }
+});
+
+// GET '/tamper'
+router.get('/tamper', function(req, res, next) {
+
+    // Invalidate chain by mssing with the data in block 2
+    /*
+        Delete this next line, save, restart the server, visit '/' to restore the blockchain, and then visit this 'tamper' uri again.
+        There is no issue and the block chain is valid.
+    */
+    dataBlockChain.chain[2].data = [new Data("I've tampered", "with the data")];
+
+    // Get the result of validation
+    const validationResult = dataBlockChain.isChainValid();
 
     // Set header type
     res.setHeader('Content-Type', 'application/json');
